f236835cd756993a0b1024fe9d9dd77d97978361
ppak10 <37542469+ppak10@users.noreply.github.com>
2018-10-03 15:24:38 -0400

    Bootstrap
    Commit adds Bootstrap functionality to Webpack but need further work to reduce file size

--- webpack.config.js
+++ webpack.config.js
@@ -22,9 +22,26 @@ module.exports = {
       {
         test:/\.scss$/,
         use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
+          {
+            loader: 'style-loader', // inject CSS to page
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS modules
+          },
+          {
+            loader: 'postcss-loader', // Run post css actions
+            options: {
+              plugins: function () { // post css plugins, can be exported to postcss.config.js
+                return [
+                  require('precss'),
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }
         ]
       }
     ]
