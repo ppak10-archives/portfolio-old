9e2ba2c948ae4413576cd6292128ca1b7cc7ab8c
ppak10 <37542469+ppak10@users.noreply.github.com>
2018-10-05 13:06:37 -0400

    Redux Devtools Extension
    Commit allows for Redux Devtools functionality with middleware through adding compose and enhancers

--- flask-javascript/src/store.js
+++ flask-javascript/src/store.js
@@ -4,20 +4,35 @@
 // ----------------------------------------------------------------------------
 
 // Module Import --------------------------------------------------------------
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 // ----------------------------------------------------------------------------
 
 // Reducer Import -------------------------------------------------------------
 import rootReducer from './reducers';
 // ----------------------------------------------------------------------------
 
+// Compose Enhancers ----------------------------------------------------------
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+// ----------------------------------------------------------------------------
+
+// Enhancer -------------------------------------------------------------------
+const enhancer = composeEnhancers(
+  applyMiddleware(
+    thunkMiddleware
+  ),
+  // other store enhancers if any
+);
+// ----------------------------------------------------------------------------
+
 // Configure Store Function ---------------------------------------------------
-function configureStore(preloadedState) {
-  return createStore(
-    rootReducer,
-    preloadedState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+function configureStore( preloadedState ) {
+  return createStore( rootReducer, enhancer )
 }
 // ----------------------------------------------------------------------------
 
