42af669db7904db4a2924deca9d6ee539a2b85a7
ppak10 <37542469+ppak10@users.noreply.github.com>
2018-10-08 19:42:32 -0400

    Navigation Bar Dropdown
    Commit updates repo to correctly import Bootstrap and its popper.js and jquery dependencies to enable dropdowns in navigation bar

--- flask-javascript/src/components/NavigationBar/index.js
+++ flask-javascript/src/components/NavigationBar/index.js
@@ -10,11 +10,61 @@ import { NavLink } from 'react-router-dom';
 
 // Navigation Bar Component ---------------------------------------------------
 const NavigationBar = () => (
-  <nav className = "navbar navbar-light bg-light">
+  <nav className = "navbar navbar-expand-lg navbar-light bg-light">
+
+    { /* Welcome Page Link */ }
     <NavLink to = "/" className = "navbar-brand">Welcome</NavLink>
-    <NavLink to = "/home">Home</NavLink>
-    <NavLink to = "/reddit">Reddit</NavLink>
-    <NavLink to = "/about">About</NavLink>
+
+    { /* Navigation Bar Collapse Button */ }
+    <button className = "navbar-toggler" type = "button"
+      data-toggle = "collapse" data-target = "#navbarSupportedContent"
+      aria-controls = "navbarSupportedContent" aria-expanded = "false"
+      aria-label = "Toggle navigation">
+      <span className = "navbar-toggler-icon"></span>
+    </button>
+
+    { /* Navigation Bar Collapsable Content */ }
+    <div className = "collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className = "navbar-nav mr-auto">
+
+        { /* Home Page Link */ }
+        <li className = "nav-item">
+          <NavLink className = "nav-link" to = "/home">Home</NavLink>
+        </li>
+
+        { /* Examples Dropdown Link */ }
+        <li className = "nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown"
+            role="button" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+            Examples
+          </a>
+
+          { /* Dropdown Links */ }
+          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+
+            { /* React Examples Divider */ }
+            <h5 className="col">React</h5>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" href="#">Action</a>
+            <a className="dropdown-item" href="#">Another action</a>
+
+            { /* Redux Examples Divider */ }
+            <h5 className="col">Redux</h5>
+            <div className="dropdown-divider"></div>
+
+            { /* Reddit Example */ }
+            <NavLink className = "dropdown-item nav-link" to = "/reddit">
+              Reddit
+            </NavLink>
+          </div>
+        </li>
+
+        <li className = "nav-item">
+          <NavLink className = "nav-link" to = "/about">About</NavLink>
+        </li>
+      </ul>
+    </div>
   </nav>
 )
 // ----------------------------------------------------------------------------
--- flask-javascript/src/index.js
+++ flask-javascript/src/index.js
@@ -4,6 +4,7 @@
 // ----------------------------------------------------------------------------
 
 // Module Imports -------------------------------------------------------------
+import 'bootstrap';                                                             // Imported for popper.js and jquery imports
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Switch } from 'react-router-dom';
@@ -18,5 +19,5 @@ import Root from './containers/Root.js';
 // ----------------------------------------------------------------------------
 
 // Render Method --------------------------------------------------------------
-render(<Root />, document.getElementById('root'));                              // Renders to document element with id of 'root'
+render(<Root />, document.getElementById('root'));                              // Renders to element with id of 'root'
 // ----------------------------------------------------------------------------
