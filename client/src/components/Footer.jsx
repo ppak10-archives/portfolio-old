a1ff9abf99eec83b5446d045181bc8f201e10730
Peter <ppak10@gmail.com>
2018-11-25 21:30:39 -0500

    Footer
    Commit adds footers for different types of pages. Closes #4.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 const Footer = () => (
   <div className = "footer">
     <p>Peter Pak 2018</p>
+    <div>
+      <span>
+        Icons borrowed from&nbsp;
+        <FontAwesomeIcon icon = {['fab', 'font-awesome-flag']} />
+        &nbsp;Font Awesome
+      </span>
+    </div>
   </div>
 );
 
--- client/src/components/ParallaxFooter.jsx
+++ client/src/components/ParallaxFooter.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+/**
+ * Components
+ */
+import Footer from './Footer.jsx';
+
 const ParallaxFooter = () => (
   <div className = "parallax-group" id = "parallax-footer">
     <div className = "parallax-layer parallax-base">
-      <p className = "title">Peter Pak 2018</p>
+      <Footer />
     </div>
   </div>
 );
